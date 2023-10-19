005e5aec92de20ab9ceab315212be75f9e087010
GabrielTI20 <120021921+GabrielTI20@users.noreply.github.com>
2023-10-19 11:49:09 -0300

    Add files via upload

---
+++ Marles-main/script/contatos.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById("info")//variavel que não pode ser alterada
+//var formularioVar = document.getElementById("info") // pode ser alterada
+//let formularioLet = document.getElementById("info") // e temporario
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault()// desligando o eveto padrão do formulario
+
+    var nome = document.getElementById("nome").value
+    var email = document.getElementById("email").value
+    var assunto = document.getElementById("assunto").value
+    var mensagem = document.getElementById("mensagem").value
+
+
+
+
+    window.location.href =
+        "envio.html" +
+        "?nome=" + encodeURIComponent(nome) +
+        "&email" + encodeURIComponent(email) +
+        "&assunto" + encodeURIComponent(assunto) +
+        "&mensagem" + encodeURIComponent(mensagem)
+
+
+
+
+    console.log(nome)
+    alert(nome + " | " + email + " | " + assunto + " | " + mensagem)
+})
+
+
